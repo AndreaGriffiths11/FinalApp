0668ed3c2ea4848ff13d72c13b5eee9a4f04cd6d
andreagriffiths11 <andreagriffiths11@gmail.com>
2017-05-02 12:32:00 -0400

    blood

---
+++ app/src/main/java/com/example/android/inventoryapp/InventoryDbHelper.java
@@ -0,0 +1,51 @@
+package com.example.android.inventoryapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
+import com.example.android.inventoryapp.InventoryContract.InventoryEntry;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class InventoryDbHelper extends SQLiteOpenHelper {
+
+    public static final String LOG_TAG = InventoryDbHelper.class.getSimpleName();
+
+    private static final String DATABASE_NAME = "inventory.db";
+
+    private static final int DATABASE_VERSION = 1;
+
+    private Context mContext;
+
+    public InventoryDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_INVENTORY_TABLE = "CREATE TABLE " + InventoryEntry.TABLE_NAME + " ("
+                + InventoryEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + InventoryEntry.COLUMN_ITEM_NAME + " TEXT NOT NULL, "
+                + InventoryEntry.COLUMN_ITEM_QUANT + " INTEGER NOT NULL DEFAULT 0, "
+                + InventoryEntry.COLUMN_ITEM_PRICE + " INTEGER NOT NULL DEFAULT 0,"
+                + InventoryEntry.COLUMN_ITEM_PHOTO + " TEXT);";
+
+
+        db.execSQL(SQL_CREATE_INVENTORY_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+}
+
+
